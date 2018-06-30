1f221672c23228301c65f69d14e14a1707db24af
Daniel Bower <drb4637@gmail.com>
2018-06-30 16:16:41 -0400

    added the main app and the initial web page

---
+++ App.js
@@ -0,0 +1,13 @@
+var exp = require('express');
+var http = require('http'); 
+var path = require('path'); 
+
+var app = exp(); 
+var serv = http.createServer(app); 
+
+var port = 1234; 
+serv.listen(port, ()=> {
+	console.log('listening on port:' + port); 
+}); 
+
+app.use(exp.static(path.join(__dirname, 'www')));
